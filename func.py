42d5d92bed8cc2bdf70647c712f7a2c6a51d6d2a
Gabriel Belo <43821561+gabrielbelolima@users.noreply.github.com>
2023-11-24 10:52:28 -0300

    adiciona funções de feriado e atualiza post_date()

--- func.py
+++ func.py
@@ -166,6 +166,88 @@ def todays_tt():
 
     return tt_list
 
+#- create_tree_feriado
+def create_tree_feriado(year=None, month=None, url = 'http://www.supercalendario.com.br/feriados/'):
+
+    # sets month and year for request ()
+    str_date = get_date(year, month)[:4]
+
+    # Scrap's Resquest
+    url_r = url+'{}'.format(str_date)
+    #print(url_r)
+    with requests.get(url_r) as r:
+        #print('DEBUG - status code: ',r.status_code)
+        r.encoding = 'UTF-8'
+        text = r.text  # string
+        #content = r.content # bytes
+
+    # generates html tree from html string
+    from lxml import html
+    tree = html.fromstring(text)
+    return tree
+
+#- get_list_feriado_futuro()
+def get_list_feriado_futuro(year = None, now = now):
+    
+    today = now.date()
+    
+    if year == None:
+        year = now.year
+    #print('feriados de ', year)
+    
+    url_feriado = 'http://www.supercalendario.com.br/feriados/'
+    tree = create_tree_feriado(year = year, url = url_feriado)
+
+    list_contents = [y.split(' - ') for y in [x.text_content() for x in tree.find_class('holidayDate')[1:]]]
+    list_feriado  = [f[0] for f in list_contents]
+    list_wd       = [f[1] for f in list_contents]
+    list_content  = [f[2] for f in list_contents]
+    
+    
+    from pandas import to_datetime
+    from dateutil.relativedelta import relativedelta
+
+    rd = (relativedelta(years = year) - relativedelta(years = 1900))  
+    list_feriado = [(to_datetime(f, format='%d/%m') + rd).date() for f in list_feriado]
+    list_distancia_feriado = [(d - today).days for d in list_feriado]
+    list_feriados_futuro = [x for x in list_distancia_feriado if x>0]
+    
+    if  len(list_feriados_futuro) == 0:
+        print('DEBUG: feriados de year+1')
+        return get_list_feriado_futuro(year = year+1)
+           
+    next_holiday_in_days = min(list_feriados_futuro)
+    next_holiday_idx = list_distancia_feriado.index(next_holiday_in_days)
+    
+    list_content = [list_feriado[next_holiday_idx], list_wd[next_holiday_idx], list_content[next_holiday_idx], next_holiday_in_days]
+    
+    return list_content
+
+#- get_content_feriado()
+def get_content_feriado(year = None, now = now):
+
+    list_content = get_list_feriado_futuro(year=year, now = now)
+    feriado  = list_content[0]
+    wd       = list_content[1]
+    content  = list_content[2]
+    
+    next_holiday_in_days = list_content[3]
+    
+    dt_month = dict(
+            zip(range(1, 13), [
+                'janeiro', 'fevereiro', 'marco', 'abril', 'maio', 'junho', 'julho',
+                'agosto', 'setembro', 'outubro', 'novembro', 'dezembro'
+            ]))
+
+    str_dt_feriado = feriado.strftime(f'%d de {dt_month[feriado.month]} de %Y')
+    str_content_feriado = f"\nFaltam {next_holiday_in_days} dias para o próximo feriado: O {content}, em {str_dt_feriado}!"
+    extra_feriado = "\nE se prepara pro feriadão que o {} cai numa {}!🌞😎".format(content, wd)
+
+    if wd in ["Segunda-feira","Sexta-feira"]:
+        str_content_feriado = str_content_feriado+extra_feriado
+    
+    #print(str_content_feriado)
+    return str_content_feriado
 
 #- format_post()
 def format_post():
@@ -176,13 +258,18 @@ def format_post():
     if len(ttt_aux) > 1:
         txt = txt.replace(',{}'.format(txt.split(',')[-1]),
                           ' e{}'.format(txt.split(',')[-1]))
+        
+    # Carrega texto sobre o próximo feriado!   
+    str_feriados = get_content_feriado()
+
+    
     # - Substitui texto específico
     txt = txt.replace('Dia do Deficiente Físico','Dia da Pessoa com Deficência Física')
 
     if txt == '':
-        return 'Hoje não temos datas comemorativas! 😥'
+        return str_feriados + '\nHoje não temos datas comemorativas! 😥 '
     else:
-        return 'Hoje é {}'.format(txt)
+        return str_feriados + '\n\nHoje é {}.'.format(txt)
 
 
 #- post_date()
