b7aab16aef67ad38a0a4e84132ed2114552be72e
Gabriel Belo <43821561+gabrielbelolima@users.noreply.github.com>
2024-10-24 08:55:20 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,18 +3,5 @@
 
 from func import * 
 
-a = os.environ['CONSUMER_KEY']
-b = os.environ['CONSUMER_SECRET']
-c = os.environ['ACCESS_TOKEN']
-d = os.environ['ACCESS_TOKEN_SECRET']
-
-print(
-str(a), 
-str(b), 
-str(c), 
-str(d), 
-sep = '\n' 
-)
-
 if __name__ == "__main__":
    post_date()
